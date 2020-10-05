d43ef32437074d92144006b36c8635acded9edcd
Brice Yuuki Gibson <byg160030@utdallas.edu>
2020-10-05 15:00:37 -0500

    Initial Unity Project Commit
    Project 02A

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+
+    private void Update()
+    {
+        // Increase Score
+        //TODO replace with real implementation later
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        // Exit Level
+        //TODO bring up popup menu for navigation
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+    public void IncreaseScore(int scoreIncrease)
+    {
+        // increase score
+        _currentScore += scoreIncrease;
+        // update score display, so we can see the new score
+        _currentScoreTextView.text =
+            "Score: " + _currentScore.ToString();
+    }
+    public void ExitLevel()
+    {
+        // compare score to high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            // save current score as new high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        // load new level
+        SceneManager.LoadScene("MainMenu");
+    }
+}
