bb0450f3703f7a7fb0cbb92283c66191e65ca109
Brice Yuuki Gibson <byg160030@utdallas.edu>
2020-10-17 01:53:09 -0500

    Movement Working
    Movement is now working but got rid of pause menu

---
+++ Assets/Scripts/Unused Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FPSInput))]
+[RequireComponent(typeof(FPSMotor))]
+public class PlayerController : MonoBehaviour
+{
+    FPSInput _input = null;
+    FPSMotor _motor = null;
+
+    [SerializeField] public float _moveSpeed = .1f;
+    [SerializeField] float _turnSpeed = 6f;
+    [SerializeField] float _jumpStrength = 10f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Level01Controller.GameIsPaused)
+        {
+            UnlockMouse();
+        }
+    }
+
+    private void Awake()
+    {
+        _input = GetComponent<FPSInput>();
+        _motor = GetComponent<FPSMotor>();
+    }
+
+    // Update is called once per frame
+    private void OnEnable()
+    {
+        _input.MoveInput += OnMove;
+        _input.RotateInput += OnRotate;
+        _input.JumpInput += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        _input.MoveInput -= OnMove;
+        _input.RotateInput -= OnRotate;
+        _input.JumpInput -= OnJump;
+    }
+
+    void OnMove(Vector3 movement)
+    {
+        // incorporate our move speed
+        _motor.Move(movement * _moveSpeed);
+    }
+
+    void OnRotate(Vector3 rotation)
+    {
+        // camera looks vertical, body rotates left/right
+        _motor.Turn(rotation.y * _turnSpeed);
+        _motor.Look(rotation.x * _turnSpeed);
+    }
+
+    void OnJump()
+    {
+        // apply our jump force to our motor
+        _motor.Jump(_jumpStrength);
+    }
+
+    void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
